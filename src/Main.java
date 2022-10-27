9341d8af988badbe5ec19c1e463d3d0b74b1df53
Diego Brown <dsb4k8@gmail.com>
2022-10-27 16:22:51 -0500

    utility function that checks if the words in a message could be cut out of a magazine (String)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.homeoffice.exercises.IsRandsomeNotePossible;
+
+public class Main {
+    public static void main(String[] args) {
+        IsRandsomeNotePossible ip = new IsRandsomeNotePossible();
+        System.out.println(ip.calculateWith("panic", "napkin company"));
+    }
+}
