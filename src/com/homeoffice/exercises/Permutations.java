d6da2ed0526dd2a24c5c7e07591fef1799252bab
Diego Brown <dsb4k8@gmail.com>
2022-10-29 01:16:06 -0500

    placeholder

---
+++ src/com/homeoffice/exercises/Permutations.java
@@ -0,0 +1,11 @@
+package com.homeoffice.exercises;
+
+
+public class Permutations {
+
+    public void printAllIn(String s){
+        StringBuilder base  = new StringBuilder(s.replaceAll(" ", ""));
+
+    }
+
+}
